10e649a2d611e0f525118ef7692396447e719da8
nguyennhuhung281197 <nhuhung281197@gmail.com>
2023-03-22 17:17:13 +0700

    Variable_datatype

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args){
+        int i  = 10; // khai báo biến i kiểu là int có giá trị là 10;
+        float f = 20.5f; // khai báo biến f kiểu là float có giá trị là 20.5
+        double d = 20.5; // khai báo biến  d kiểu là double có giá trị là 20.5
+        boolean b = true; // khai báo biến b kiểu boolean có giá trị true
+        char c = 'a'; // khai báo biến c kiểu char có kí tự là a
+        String s = "Hà Nội"; // khai báo biến s kiểu String có giá trị chuỗi là Hà Nội
+
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+
+    }
+}
